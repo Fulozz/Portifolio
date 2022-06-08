af7961ca2cf57b5e448250f2fa33ffc9c20fad33
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2022-06-08 09:12:01 -0300

    confirmar

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php  
+    $name = $_POST['name']
+    $visitor_eail = $_POST['email']
+    $message = $_POST
+
+    $email_from = 'thiago.sandrade0720@gmail.com'
+    $email_subject = "New Form Subjection"
+
+    $email_body = "User name: $name.\n"
+                     "User email"
+
+?>
